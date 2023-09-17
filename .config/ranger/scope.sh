9eb752f906485135ea44d4f26bd713d44c17f2d4
Izmyname <MCMXCV@proton.me>
2023-09-18 03:50:16 +0500

    disable thumbnails for audio and video

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -171,18 +171,18 @@ handle_image() {
             exit 7;;
 
         ## Video
-         video/*)
+        # video/*)
              # Get embedded thumbnail
-             ffmpeg -i "${FILE_PATH}" -map 0:v -map -0:V -c copy "${IMAGE_CACHE_PATH}" && exit 6
+             #ffmpeg -i "${FILE_PATH}" -map 0:v -map -0:V -c copy "${IMAGE_CACHE_PATH}" && exit 6
              # Get frame 10% into video
-             ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-             exit 1;;
+            # ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+             #exit 1;;
 
         ## Audio
-         audio/*)
+         #audio/*)
              # Get embedded thumbnail
-             ffmpeg -i "${FILE_PATH}" -map 0:v -map -0:V -c copy \
-               "${IMAGE_CACHE_PATH}" && exit 6;;
+             #ffmpeg -i "${FILE_PATH}" -map 0:v -map -0:V -c copy \
+               #"${IMAGE_CACHE_PATH}" && exit 6;;
 
         ## PDF
          application/pdf)
